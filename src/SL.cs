ce6daf8801c918dc5a9a35ad93323d6b6aa065d2
Sinai <49360850+sinai-dev@users.noreply.github.com>
2021-08-23 18:30:48 +1000

    Bump version

--- src/SL.cs
+++ src/SL.cs
@@ -23,7 +23,7 @@ namespace SideLoader
         // Mod Info
         public const string GUID = "com.sinai.SideLoader";
         public const string MODNAME = "SideLoader";
-        public const string VERSION = "3.4.11";
+        public const string VERSION = "3.5.0";
 
         // Core Folders
         internal static string SL_FOLDER => s_slFolder ?? (s_slFolder = Path.GetDirectoryName(typeof(SL).Assembly.Location));
